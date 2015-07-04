309cd38fa12f5c6d623129de927d2e9241ebbb8a
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2015-07-04 04:52:35 -0400

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -5,8 +5,8 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#064BCE',
+    lineColor: '#0776FB'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
